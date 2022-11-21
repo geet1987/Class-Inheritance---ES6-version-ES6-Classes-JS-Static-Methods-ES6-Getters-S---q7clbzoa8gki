0ecd17bfaba80d2c25b625f2a2f9463473aaf4a9
geet1987 <52599381+geet1987@users.noreply.github.com>
2022-11-21 09:22:53 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,17 @@
 // import using require
-
+import shape from ./shape;
 // declare class
-
+class Circle extends Shape {
+    constructor(x, y, radius) {
+        super(x, y, radius);
+        this.radius = radius
+    }
+ 
+    getArea() {...}
+ 
+}
+ 
+let circle = new Circle(1,2,3);
 
 // export class using module.exports
+default export circle;
