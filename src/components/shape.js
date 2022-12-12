c114e827aa65e05207758f20a9cfdbaa2a4034cc
geet1987 <52599381+geet1987@users.noreply.github.com>
2022-12-12 13:29:25 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,14 +1,20 @@
-// declare class
-Rectangle = class Shape {
-  drawShape(height, width) {
-    this.height = height;
-    this.width = width;
-  }
-  calculateArea(){
-  }
-  color(){
-  }
-};
-
-// export class using module.exports
-default export shape;
+class Shape{ 
+     constructor(name){ 
+        this.name; 
+  
+     } 
+  
+     drawShape(){ 
+         return`${this.name} is drawn`; 
+     } 
+  
+     calculateArea(length,width){ 
+         return 2*(length*width); 
+          
+     } 
+     color(color){ 
+         return `${color} is Applied on ${this.name}`; 
+     } 
+      
+ } 
+ module.exports = Shape; 
